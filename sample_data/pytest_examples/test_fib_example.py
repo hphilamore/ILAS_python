2cb40c32e555872a0bd9905e9291a64e773c3358
hphilamore <hemmaphilamore@gmail.com>
2018-01-04 00:46:27 +0900

    testing seminar update

---
+++ sample_data/pytest_examples/test_fib_example.py
@@ -0,0 +1,22 @@
+import pytest
+
+def f(n): 
+        "Compute the nth Fibonacci number using recursion"
+        if n == 0:
+            return 0  # Breaks out of the recursion loop
+        elif n == 1:
+            return 1  # Breaks out of the recursion loop
+        else:
+            return f(n - 1) + f(n - 2)  # Calls f for n-1 and n-2 (recursion) 
+
+def test_fibonacci():
+	print("hi")
+	assert f(0) == 0
+	assert f(1) == 1
+	assert f(2) == 1
+	assert f(3) == 2
+	assert f(10) == 55
+	assert f(15) == 610, "Fail: Fibonacci number, term 15, is incorrect"
+
+def test_2():
+	assertEqual(f(1),f(2))
--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-
-a = np.loadtxt('sample_data/sample_data_seminar10.dat')
-
-a = int(a[0][0])
-
-a = 1 / a
-
